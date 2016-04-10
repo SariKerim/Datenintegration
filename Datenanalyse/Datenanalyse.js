473450e6cb58d8b15a6d07850f4dbf52f7182e7a
SariKerim <ks146@hdm-stuttgart.de>
2016-04-11 01:09:02 +0200

    Eigene Version rausgeworfen. Einpflegen von Katharina Köhlers Variante.

--- Datenanalyse/Datenanalyse.js
+++ Datenanalyse/Datenanalyse.js
@@ -1,5 +1,58 @@
-// REQUIRED MODULES
-fs = require('fs');
+// REQUIRED GLOBALS
+var fs = require('fs');
+var resultStream = fs.createWriteStream('results.txt');
+var resultArray = [];
+
+//SKRIPTSTART
+
+resetFiles();
+console.log("Lese Daten...");
+
+fs.readFile('../Liste_PPN-ExNr_HSHN-libre.csv', 'utf-8', function(err, inhalt) {
+	if(err) {
+		return console.log(err);
+	}
+	
+	var lines = inhalt.split(/\r?\n/);
+	
+	for(var i = 1; i < lines.length; i++) {
+		
+		var line = lines[i];
+		var tokens = line.split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+		
+		if (tokens.length != 5) {
+			error("Komische Zeile (" + (i+1) + "): " + line);
+		} else {
+			
+			var exemplar = {
+				ppn: checkPPN(tokens[0]),
+				exemplar: tokens[1],
+				signatur: tokens[2],
+				barcode: tokens[3],
+				sigel: tokens[4]
+			};
+			resultArray.push(exemplar);
+		}
+	}
+	
+	output("output.csv", JSON.stringify(resultArray, null, 2));
+
+	console.log("Anzahl Zeilen: " + (lines.length - 2));
+	console.log("Anzahl vollständiger Datensätze: " + (resultArray.length));
+	console.log("Analysiere Daten...");
+	
+	// Original Author: Katharina Köhler
+	var allEntries = resultArray.length;
+	resultStream.write(
+	getUniqueValues('sigel').length + ' Sigel sind verfügbar: ' + getUniqueValues('Sigel') + '\n' +
+	getUniqueValues('ppn').length + ' unterschiedliche PPNs sind vorhanden. \n' +
+	getUniqueValues('exemplar').length + ' einzigartige Exemplarnummern -> ' + ((allEntries) - (getUniqueValues('exemplar').length)) + ' haben einen Fehler. \n' +
+	getUniqueValues('barcode').length + ' unterschiedliche Barcodes -> ' + ((allEntries) - (getUniqueValues('barcode').length)) + ' haben einen Fehler. \n' +
+	getUniqueValues('signatur').length + ' unterschiedliche Signaturen \n' +
+	'Histogram for the amount of exemplars per location:\n' + JSON.stringify(getExemplarsForKey('sigel'), 0, 2) + 
+	'\nHistogram for the amount of exemplars per title:\n' + JSON.stringify(getExemplarsForKey('ppn'), 0, 2) 
+	);
+});
 
 // HILFSFUNKTIONEN
 function writeToFile(filename, data) {
@@ -13,8 +66,7 @@ function writeToFile(filename, data) {
 
 function resetFiles() {
 	fs.writeFileSync("output.csv", "");
-	fs.writeFileSync("ppn.txt", "");
-	fs.writeFileSync("signaturen.txt", "");
+	fs.writeFileSync("results.txt", "");
 	fs.writeFileSync("error.log", "");
 }
 
@@ -41,122 +93,36 @@ function checkPPN(ppn) {
 	}
 };
 
-//SKRIPTSTART
-
-resetFiles();
-
-fs.readFile('../Liste_PPN-ExNr_HSHN-libre.csv', 'utf-8', function(err, inhalt) {
-	if(err) {
-		return console.log(err);
-	}
-	
-	var lines = inhalt.split(/\r?\n/);
-	var result = [];
-	
-	for(var i = 1; i < lines.length; i++) {
-		
-		var line = lines[i];
-		var tokens = line.split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
-		
-		if (tokens.length != 5) {
-			error("Komische Zeile (" + (i+1) + "): " + line);
-		} else {
-			
-			var exemplar = {
-				ppn: checkPPN(tokens[0]),
-				exemplar: tokens[1],
-				signatur: tokens[2],
-				barcode: tokens[3],
-				sigel: tokens[4]
-			};
-			result.push(exemplar);
+// Original Author: Katharina Köhler
+function getUniqueValues(keyName) {
+	var seen = {}
+	var tmpArray = resultArray.map(function (entry) {
+		return entry[keyName];
+	});
+	return tmpArray.filter(function(x) {
+		if(seen[x]) {
+			return;
 		}
-	}
-	
-	output("output.csv", JSON.stringify(result, null, 2));
-
-	console.log("Anzahl Zeilen: " + (lines.length - 2));
-	console.log("Anzahl vollständiger Datensätze: " + (result.length));
-	
-	getAllUniqueSignatures(result);
-	getAllUniquePPN(result);
-});
-
-// Funktion zum auslesen aller einzigartigen Signaturen
-function getAllUniqueSignatures(data) {
-	var uniqueSignatures = [];
-	var allSignatures = [];
-	
-	// Zunächst schieben wir alle Signaturen in ein eigenes Array
-	for(var i = 0; i < data.length; i++) {
-		allSignatures.push(data[i].signatur);
-	}
-	
-	// Anschließend wird jede Signatur aus allSignatures die noch nicht in uniqueSignatures vorkommt
-	// dorthin kopiert.
-	for(var i = 0; i<allSignatures.length; i++) {
-		if(uniqueSignatures.indexOf(allSignatures[i]) != -1) {
-			continue;
-		} else {
-			uniqueSignatures.push(allSignatures[i]);
-		}
-	}
-	
-	output("signaturen.txt", JSON.stringify(uniqueSignatures));
-	
-	console.log("Anzahl einzigartiger Signaturen: " + (uniqueSignatures.length));
+		seen[x] = true;
+		return x;
+	});
 }
 
-function getAllUniquePPN(data) {
-	var uniquePPNs = [];
-	var allPPNs = [];
+// Original Author: Katharina Köhler
+function getExemplarsForKey(key) {
+	var tmpArray = {};
 	
-	// Zunächst schieben wir alle Signaturen in ein eigenes Array
-	for(var i = 0; i < data.length; i++) {
-		allPPNs.push(data[i].ppn);
-	}
-	
-	// Anschließend wird jede PPN aus allPPNs die noch nicht in uniquePPNs vorkommt
-	// dorthin kopiert.
-	for(var i = 0; i<allPPNs.length; i++) {
-		if(uniquePPNs.indexOf(allPPNs[i]) != -1) {
-			continue;
-		} else {
-			uniquePPNs.push(allPPNs[i]);
+	for(var entry in resultArray) {
+		var groupKey = resultArray[entry][key];
+		
+		if(groupKey in tmpArray) {
+			tmpArray[groupKey] += 1;
+		}
+		else {
+			tmpArray[groupKey] = 1;
 		}
 	}
 	
-	output("ppn.txt", JSON.stringify(uniquePPNs));
-	
-	console.log("Anzahl einzigartiger PPNs: " + (uniquePPNs.length));
+	return tmpArray;
 }
 
-// Auslesen aller vorhandenen PPNs und wie oft diese vorkommen
-function getCountPerPPN(data) {
-	
-	var duplicates = []
-	
-	for(var i = 0; i<1000; i++) {
-		var ppnCount = 1;
-		
-		for(var j = 0; j<1000; j++) {
-			if(i==j) {
-				continue;
-			} else {
-				if(data[i].ppn == data[j].ppn) {
-					ppnCount++;
-				}
-			}		
-		}
-		
-		var ppn = {ppn: data[i].ppn, count: ppnCount};
-		duplicates.push(ppn)
-	}
-
-	duplicates.forEach(function(element, index, array) {
-		if(element.count > 1) {
-			console.log("PPN: " + element.ppn);
-			console.log("Anzahl: " + element.count);
-		}
-	})
-}
